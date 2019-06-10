9b9701847f875880fa1364663d2c5aab1a1c4c1b
AshokMeiyanathan <ashok.nathen@gmail.com>
2019-06-10 00:46:57 -0400

    Included Selenium scripts

--- AIMavenProject/src/test/java/com/qa/tests/DeomTest.java
+++ AIMavenProject/src/test/java/com/qa/tests/DeomTest.java
@@ -1,6 +1,8 @@
 package com.qa.tests;
 
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -8,6 +10,11 @@ public class DeomTest {
 
 	@Test
 	public void sum(){
+		System.setProperty("webdriver.gecko.driver","C:\\Firefox Driver\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.google.com/");
+		System.out.println(driver.getTitle());
+		driver.quit();
 		int a=10;
 		int b=10;
 		System.out.println("Sum");
